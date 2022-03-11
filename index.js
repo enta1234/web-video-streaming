e173711a5c71ff891004dd95f77b987bf2efce96
chantatha.p <chantaha.p@gmail.com>
2022-03-11 10:09:37 +0700

    added: 1st commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const fs = require('fs')
+
+const app = express()
+
+app.use(express.json({encode: true }))
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/viewer.html");
+});
+
+app.get('/video', (req, res) => {
+  const range = req.headers.range;
+  if (!range) {
+    res.status(400).send("Requires Range header");
+  }
+  const vdoSize = fs.statSync('./v1.mp4').size
+  
+  const CHUNK_SIZE = 10 ** 6 * 20;
+  const start = Number(range.replace(/\D/g, ""));
+  const end = Math.min(start + CHUNK_SIZE, vdoSize - 1);
+  const contentLength = end - start + 1;
+  const headers = {
+    "Content-Range": `bytes ${start}-${end}/${vdoSize}`,
+    "Accept-Ranges": "bytes",
+    "Content-Length": contentLength,
+    "Content-Type": "video/mp4",
+  };
+  res.writeHead(206, headers);
+
+  const vdoStream = fs.createReadStream('./v1.mp4')
+  vdoStream.pipe(res)
+})
+
+app.listen(3000, console.log('start at 3000'))
